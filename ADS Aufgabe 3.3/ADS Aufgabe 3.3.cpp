cdcb3529850068010336750180e0ac1c2896c3b5
m00ny <TSCxxXMONSTAXxx@gmail.com>
2017-07-16 18:14:25 +0200

    Update ADS Aufgabe 3.3.cpp

--- ADS Aufgabe 3.3/ADS Aufgabe 3.3.cpp
+++ ADS Aufgabe 3.3/ADS Aufgabe 3.3.cpp
@@ -1,9 +1,10 @@
-// FERTIG - Severin Meier
+//
 //
 
-#include "stdafx.h"
 #include <iostream>
-#include "TowerMap.h"
+#include <stdio.h>
+#include <tchar.h>
+#include <vector>
 
 bool showsteps = true; //CHANGE THIS TO SPEED UP THE PROCESS OR TO SHOW EVERY STEP!
 int Steps;
@@ -11,88 +12,81 @@ int Steps;
 std::vector<std::vector<int>> _tower(3);
 
 void printTowers();
-void setzeTurm(int hight, int from=1, int to=3, int with=2);
+void setTower(int hight, int from=1, int to=3, int with=2);
 
 int main()
 {
+        int disks;
+
+        std::cout << "How many disks should the tower have? " << std::endl;
+        std::cin >> disks;
+
+        for (int i = 0; i<disks; i++)
+        {
+                _tower[0].push_back(disks - i);
+        }
+
+        printTowers();
+        std::cout << "##############################################" << std::endl;
+
+        setTower(_tower[0].size());
+
+        std::cout << "##############################################" << std::endl;
+        printTowers();
+        std::cout << "Schritte: " << Steps << std::endl;
 
 
-	int Scheiben;
-	
-
-	std::cout << "How many disks should the tower have? " << std::endl;
-	std::cin >> Scheiben;
-
-	for (int i = 0; i<Scheiben; i++)
-	{
-		_tower[0].push_back(Scheiben - i);
-	}
-
-	printTowers();
-	std::cout << "##############################################" << std::endl;
-
-	setzeTurm(_tower[0].size());
-
-	std::cout << "##############################################" << std::endl;
-	printTowers();
-	std::cout << "Schritte: " << Steps << std::endl;
-
-
-	system("pause");
-	return 0;
+        system("pause");
+        return 0;
 }
-
 
 
 void printTowers()
 {
-	system("cls");
+        system("cls");
 
-	int tmp = 0;
+        int tmp = 0;
 
-	for (int i = 0; i < _tower.size(); i++)
-	{
-		if (_tower[i].size() > tmp)
-		{
-			tmp = _tower[i].size();
-		}
-	}
-
-	for (int i = tmp; i >= 0; i--)
-	{
-		for (int j = 0; j < _tower.size(); j++)
-		{
-			if ( i < _tower[j].size())
-				std::cout << "[" << _tower[j][i] << "]\t";
-			else
-				std::cout << "\t";
-		}
-		std::cout << std::endl;
-	}
-	std::cout << "tow1\ttow2\ttow3" << std::endl;
-
+        for (int i = 0; i < _tower.size(); i++)
+        {
+                if (_tower[i].size() > tmp)
+                {
+                        tmp = _tower[i].size();
+                }
+        }
+        for (int i = tmp; i >= 0; i--)
+        {
+                for (int j = 0; j < _tower.size(); j++)
+                {
+                        if ( i < _tower[j].size())
+                                std::cout << "[" << _tower[j][i] << "]\t";
+                        else
+                                std::cout << "\t";
+                }
+                std::cout << std::endl;
+        }
+        std::cout << "tow1\ttow2\ttow3" << std::endl;
 }
 
-void setzeTurm(int hight,int from,int to,int with)
+void setTower(int hight,int from,int to,int with)
 {
-	
-	if ( hight > 0)
-	{
-		setzeTurm((hight - 1), from, with, to);
-		_tower[to - 1].push_back(_tower[from - 1].back()
-		);
-		_tower[from - 1].pop_back();
-		
-		Steps++;
-		std::cout << "Step: " << Steps << " \n\n";
-		
-		if (showsteps)
-		{
-			printTowers();
-			system("pause");
-		}
-		setzeTurm(hight - 1, with, to, from);
+        if ( hight > 0)
+        {
+                setTower((hight - 1), from, with, to);
+                _tower[to - 1].push_back(_tower[from - 1].back()
+                );
+                _tower[from - 1].pop_back();
+
+                Steps++;
+                std::cout << "Step: " << Steps << " \n\n";
+
+                if (showsteps)
+                {
+                        printTowers();
+                        system("pause");
+                }
+                setTower(hight - 1, with, to, from);
 
 
-	}		
+        }
 }
